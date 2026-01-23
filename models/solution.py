c9e290dab7ecaafa938b460a9de0b003f390fe32
twinsmaram-hub <twinsmaram@gmail.com>
2026-01-23 20:11:03 +0300

    modify solution model

--- models/solution.py
+++ models/solution.py
@@ -8,7 +8,11 @@ class SolutionModel(BaseModel):
 
   id = Column(Integer, primary_key=True, index=True)
   content = Column(String, nullable=False)
+  is_AI = Column(bool)
 
   # relationships
   problem_id = Column(Integer, ForeignKey('problems.id', ondelete="CASCADE"), nullable=False)
   problem = relationship('ProblemModel', back_populates='solutions', passive_deletes=True)
+  
+  user_id = Column(Integer, ForeignKey('users.id',  ondelete="CASCADE"), nullable=False)
+  user = relationship('UserModel', back_populates='comments')
